e4a96272f0e02d8bbe27cc1dd0a0044f08264e63
JungBin-Eom <ejb0329@gmail.com>
2021-05-04 01:52:55 +0000

    app version 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the second version of Ricky React!
+          This is the second version of Ricky React.
         </p>
         <a
           className="App-link"
